b1cc43c5f503d7b660694543aabe400ab34b0256
zikrya <zikrya8@gmail.com>
2024-10-18 18:55:31 -0400

    [REFRACTOR] removed case sensitivity

--- stocks_app/views.py
+++ stocks_app/views.py
@@ -5,6 +5,7 @@ from services.financial_data_service import FinancialDataService
 def fetch_data_view(request, symbol):
     if request.method == 'GET':
         try:
+            symbol = symbol.upper()
             message = FinancialDataService.fetch_stock_data(symbol)
             return JsonResponse({'message': message}, status=200)
         except Exception as e:
