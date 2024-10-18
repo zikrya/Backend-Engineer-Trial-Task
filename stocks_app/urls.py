0a9c841615858ca7bf53138c7324bf44bcaaed99
zikrya <zikrya8@gmail.com>
2024-10-18 19:52:49 -0400

    [FEAT] Implement into django

--- stocks_app/urls.py
+++ stocks_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import fetch_data_view, stocks_home_view
+from .views import fetch_data_view, run_backtest_view
 
 urlpatterns = [
     path('', stocks_home_view, name='stocks_home'),
     path('fetch/<str:symbol>/', fetch_data_view, name='fetch_data'),
+    path('backtest/<str:symbol>/', run_backtest_view, name='run_backtest'),
 ]
 
--- stocks_app/views.py
+++ stocks_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from services.financial_data_service import FinancialDataService
+from services.backtesting_service import BacktestingService
 
 def fetch_data_view(request, symbol):
     if request.method == 'GET':
@@ -15,6 +16,21 @@ def fetch_data_view(request, symbol):
 
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
+
+def run_backtest_view(request, symbol):
+    if request.method == 'GET':
+        initial_investment = request.GET.get('initial_investment', 10000)
+        try:
+            initial_investment = float(initial_investment)
+        except ValueError:
+            return JsonResponse({'error': 'Invalid investment amount'}, status=400)
+
+        summary = BacktestingService.run_backtest(symbol, initial_investment)
+
+        if isinstance(summary, str):
+            return JsonResponse({'error': summary}, status=400)
+
+        return JsonResponse(summary, status=200)
 
 def stocks_home_view(request):
     return HttpResponse("Welcome to the Stocks API! Use /stocks/fetch/<symbol> to get stock data.")
