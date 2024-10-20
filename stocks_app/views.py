4438aac66083b2d502530b312c876dcb4ece31d9
zikrya <zikrya8@gmail.com>
2024-10-20 04:16:07 -0400

    New Test

--- stocks_app/views.py
+++ stocks_app/views.py
@@ -78,4 +78,4 @@ def generate_report_view(request, symbol):
         return JsonResponse({'error': str(e)}, status=500)
 
 def stocks_home_view(request):
-    return HttpResponse("Welcome to the Stocks API!")
+    return HttpResponse("Welcome to the Stocks API!!")
